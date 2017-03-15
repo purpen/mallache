f431c11012ecc01d2cd134015f467690d1c24ece
llh <liheng666@foxmail.com>
2017-03-15 18:34:04 +0800

    导入城市表

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 数据库模型基类
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    //
+}
---
+++ app/Models/ChineCity.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 城市表(收货地址－－京东)
+ */
+
+namespace App\Models;
+
+class ChinaCity extends BaseModel
+{
+    public $timestamps = false;
+
+    /**
+     * 关联到模型的数据表
+     *
+     * @var string
+     */
+    protected $table = 'china_cities';
+
+    /**
+     * 获取关联信息
+     */
+    public function fetchCity($pid=0)
+    {
+        $query['pid'] = (int)$pid;
+        $query['status'] = 1;
+
+        $cities = self::where($query)->orderBy('sort', 'desc')->get();
+        return $cities;
+    }
+
+}
