a1cca1615362f3f8eebe765235af06be44e6597a
llh <liheng666@foxmail.com>
2017-03-27 14:44:31 +0800

    change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
+/*    id	int(10)	否		用户ID
+    account	varchar(20)	否		用户名
+    password	varchar(50)	否		密码
+    username	varchar(50)	是		用户名
+    email	varchar(50)	是		邮箱
+    phone	varchar(20)	否		手机号码
+    design_company_id	int(10)	否	0	设计公司ID
+    status	tinyint(1)	否	0	状态：；-1：禁用；1.激活;2...
+    item_sum	int(10)	否	0	项目数量
+    price_total	decimal(10,2)	否	0	总金额
+    price_frozen	decimal(10,2)	否	0	冻结金额
+*/
+
     /**
      * Run the migrations.
      *
@@ -20,13 +33,15 @@ class CreateUsersTable extends Migration
             $table->string('username',50)->default('');
             $table->string('email')->default('');
             $table->string('password');
+            $table->integer('design_company_id')->default(0);
             $table->tinyInteger('type')->default(0);
             $table->tinyInteger('status')->default(0);
+            $table->integer('item_sum')->default(0);
+            $table->decimal('price_total',10,2)->default(0);
+            $table->decimal('price_frozen',10,2)->default(0);
             $table->rememberToken();
             $table->timestamps();
             $table->unique('email');
-
-
         });
     }
 
