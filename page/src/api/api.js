923d3d3a6fad70e2592a47ae07187d5e41b7526f
tianshuai <tianshuaiorc@sina.com>
2017-05-02 11:20:15 +0800

    api

--- page/src/api/api.js
+++ page/src/api/api.js
@@ -15,35 +15,35 @@ export function fetch(url) {
 **/
 
 export default {
-  login: '/api/auth/login', // 登录
-  logout: '/api/auth/logout', // 退出登录
-  register: '/api/auth/register', // 注册
-  check_account: '/api/auth/phoneState/',  // 验证手机号是否存在
-  fetch_msm_code: '/api/auth/sms', // 获取手机验证码
-  user: '/api/auth/user',  // 获取用户信息
+  login: '/auth/login', // 登录
+  logout: '/auth/logout', // 退出登录
+  register: '/auth/register', // 注册
+  check_account: '/auth/phoneState/',  // 验证手机号是否存在
+  fetch_msm_code: '/auth/sms', // 获取手机验证码
+  user: '/auth/user',  // 获取用户信息
 
-  designCompany: '/api/designCompany', // 设计公司基本资料
+  designCompany: '/designCompany', // 设计公司基本资料
 
-  demandAlipay: '/api/pay/demandAliPay',  // 支付保证金
+  demandAlipay: '/pay/demandAliPay',  // 支付保证金
 
-  saveDesignItem: '/api/designItem', // 接单信息保存
-  designItems: '/api/designItem', // 查看接单列表
-  designItem: '/api/designItem/{0}', // 查看/编辑/删除接单信息
+  saveDesignItem: '/designItem', // 接单信息保存
+  designItems: '/designItem', // 查看接单列表
+  designItem: '/designItem/{0}', // 查看/编辑/删除接单信息
 
-  designCase: '/api/designCase',  // 公司案例
-  designCaseId: '/api/designCase/{0}',  // 公司案例
+  designCase: '/designCase',  // 公司案例
+  designCaseId: '/designCase/{0}',  // 公司案例
 
-  itemList: '/api/demand/itemList',  // 客户项目列表
+  itemList: '/demand/itemList',  // 客户项目列表
 
-  demand: '/api/demand',  // 添加项目领域
+  demand: '/demand',  // 添加项目领域
   demandId: '/api/demand/{0}',  // 更改项目领域
 
-  ProductDesignId: '/api/ProductDesign/{0}', // 更改产品设计基本资料
-  UDesignId: '/api/UDesign/{0}', // 更改UI设计基本资料
+  ProductDesignId: '/ProductDesign/{0}', // 更改产品设计基本资料
+  UDesignId: '/UDesign/{0}', // 更改UI设计基本资料
 
-  release: '/api/demand/release', // 发布项目
+  release: '/demand/release', // 发布项目 
 
-  asset: '/api/upload/deleteFile/{0}',  // 删除／查看图片
-  upToken: '/api/upload/upToken'  // 获取上传token
+  asset: '/upload/deleteFile/{0}',  // 删除／查看图片
+  upToken: '/upload/upToken'  // 获取上传token
 
 }
