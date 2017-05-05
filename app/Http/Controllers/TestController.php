1c112420c8dd2f444c240dbc9c646320b94748d9
llh <liheng666@foxmail.com>
2017-05-05 17:12:18 +0800

    debug

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Helper\QiniuApi;
+use App\Models\DesignCompanyModel;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -12,15 +14,25 @@ class TestController extends Controller
      */
     public function index()
     {
-        //随机字符串(回调查询)
-        $random = [];
-        for ($i = 0; $i<2; $i++){
-            $random[] = uniqid();  //获取唯一字符串
+
+        $design_s = DesignCompanyModel::select(['id','user_id'])->get();
+
+        foreach($design_s as $v){
+            $user = User::find($v->user_id);
+            $user->design_company_id = $v->id;
+            $user->save();
         }
-        $upload_url = config('filesystems.disks.qiniu.upload_url');
-        $token = QiniuApi::upToken();
-        $user_id = 1;
-        return view('test.index',compact('token' , 'upload_url' , 'random' , 'user_id'));
+
+        echo 'true';
+        //随机字符串(回调查询)
+//        $random = [];
+//        for ($i = 0; $i<2; $i++){
+//            $random[] = uniqid();  //获取唯一字符串
+//        }
+//        $upload_url = config('filesystems.disks.qiniu.upload_url');
+//        $token = QiniuApi::upToken();
+//        $user_id = 1;
+//        return view('test.index',compact('token' , 'upload_url' , 'random' , 'user_id'));
     }
 
     public function create()
