e08b6f2a58813c7360ae049a4e3538499c673497
tianshuai <tianshuaiorc@sina.com>
2017-05-08 09:59:15 +0800

    admin list

--- page/src/components/admin/company/List.vue
+++ page/src/components/admin/company/List.vue
@@ -9,13 +9,13 @@
 
         <div class="admin-menu-sub">
           <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList'}" active-class="none" :class="{'item': true, 'is-active': menuType == 0}">全部</router-link>
+            <router-link :to="{name: 'adminCompanyList'}" active-class="false" :class="{'item': true, 'is-active': menuType == 0}">全部</router-link>
           </div>
           <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList', query: {type: 1}}" active-class="none" :class="{'item': true, 'is-active': menuType === 1}">待审核</router-link>
+            <router-link :to="{name: 'adminCompanyList', query: {type: 1}}" :class="{'item': true, 'is-active': menuType === 1}" active-class="false">待审核</router-link>
           </div>
           <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList', query: {type: 8}}" active-class="none" :class="{'item': true, 'is-active': menuType === 8}">通过审核</router-link>
+            <router-link :to="{name: 'adminCompanyList', query: {type: 8}}" :class="{'item': true, 'is-active': menuType === 8}" active-class="false">通过审核</router-link>
           </div>
         </div>
 
