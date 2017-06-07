470aabc84dcceef9b3e53eb5adaec0825a780c51
llh <liheng666@foxmail.com>
2017-06-07 17:42:38 +0800

    dehug

--- app/Http/Controllers/Api/V1/ContractController.php
+++ app/Http/Controllers/Api/V1/ContractController.php
@@ -120,7 +120,7 @@ class ContractController extends BaseController
             'item_content' => 'required',
             'design_work_content'  => 'required',
             'title'  => 'required|max:20',
-            'item_stage' => 'array',
+//            'item_stage' => 'array',
         ];
 
         $messages = [
@@ -380,7 +380,7 @@ class ContractController extends BaseController
             'item_content' => 'required',
             'design_work_content'  => 'required',
             'title'  => 'required|max:20',
-            'item_stage' => 'array',
+//            'item_stage' => 'array',
         ];
 
         $messages = [
