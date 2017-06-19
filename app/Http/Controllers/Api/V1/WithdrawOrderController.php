8e475a9a0b34b7288e46a2c782b2807c00a75d25
llh <liheng666@foxmail.com>
2017-06-19 18:01:00 +0800

    change

--- app/Http/Controllers/Api/V1/WithdrawOrderController.php
+++ app/Http/Controllers/Api/V1/WithdrawOrderController.php
@@ -59,7 +59,6 @@ class WithdrawOrderController extends BaseController
 
         //可提现金额
         $cash = $this->auth_user->cash;
-        dd($amount, $cash);
         if($amount > $cash){
             return $this->response->array($this->apiError('可提现金额不足', 403));
         }
