86ca71ea26dd77514b3d8a748d50bc0e708e1152
tianshuai <tianshuaiorc@sina.com>
2017-07-25 11:01:37 +0800

    ie11 bug

--- page/src/components/admin/company/Show.vue
+++ page/src/components/admin/company/Show.vue
@@ -7,171 +7,168 @@
       <el-col :span="20">
         <div class="content">
 
-        <div class="admin-menu-sub">
-          <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList'}" active-class="false" :class="{'item': true, 'is-active': menuType == 0}">全部</router-link>
-          </div>
-          <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList', query: {type: -1}}" :class="{'item': true, 'is-active': menuType === -1}" active-class="false">待审核</router-link>
-          </div>
-          <div class="admin-menu-sub-list">
-            <router-link :to="{name: 'adminCompanyList', query: {type: 1}}" :class="{'item': true, 'is-active': menuType === 1}" active-class="false">通过审核</router-link>
-          </div>
-        </div>
-
-
-        <div class="content-box" v-loading.body="isLoading">
-
-          <div class="form-title">
-            <span>基本信息</span>
-          </div>
-
-          <div class="company-show">
-
-            <div class="item" style="height: 90px;">
-              <p class="p-key">头像</p>
-              <p class="p-val"><img v-if="item.logo_url" :src="item.logo_url" width="80" /></p>
+          <div class="admin-menu-sub">
+            <div class="admin-menu-sub-list">
+              <router-link :to="{name: 'adminCompanyList'}" active-class="false" :class="{'item': true, 'is-active': menuType == 0}">全部</router-link>
             </div>
-
-            <div class="item">
-              <p class="p-key">简称</p>
-              <p class="p-val">{{ item.company_abbreviation }}</p>
+            <div class="admin-menu-sub-list">
+              <router-link :to="{name: 'adminCompanyList', query: {type: -1}}" :class="{'item': true, 'is-active': menuType === -1}" active-class="false">待审核</router-link>
             </div>
-
-            <div class="item">
-              <p class="p-key">地址</p>
-              <p class="p-val"><span v-for="(d, index) in item.city_arr" :key="index">{{ d }} &nbsp;</span> {{ item.address }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">网址</p>
-              <p class="p-val">{{ item.web }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">规模</p>
-              <p class="p-val">{{ item.company_size_val }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">分公司</p>
-              <p class="p-val">{{ item.branch_office }}</p>
-            </div>
-
-            <div class="item des">
-              <p class="p-key">公司简介</p>
-              <p class="p-val">{{ item.company_profile }}</p>
-            </div>
-
-            <div class="item des">
-              <p class="p-key">专业优势</p>
-              <p class="p-val">{{ item.professional_advantage }}</p>
-            </div>
-
-            <div class="item des">
-              <p class="p-key">荣誉奖项</p>
-              <p class="p-val">{{ item.awards }}</p>
-            </div>
-
-          </div>
-
-          <div class="form-title">
-            <span>认证信息</span>
-          </div>
-
-          <div class="company-show">
-            <div class="item">
-              <p class="p-key">企业名称</p>
-              <p class="p-val">{{ item.company_name }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">企业证件类型</p>
-              <p class="p-val">{{ item.company_type_val }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">企业营业执照</p>
-              <p class="p-val"><a v-for="(d, index) in item.license_image" :key="index" :href="d.file" target="_blank">{{ d.name }} </a></p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">注册号</p>
-              <p class="p-val">{{ item.registration_number }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">法人姓名</p>
-              <p class="p-val">{{ item.legal_person }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">法人证件类型</p>
-              <p class="p-val">{{ item.document_type_val }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">法人证件附件</p>
-              <p class="p-val"><a v-for="(d, index) in item.document_image" :key="index" :href="d.file" target="_blank">{{ d.name }} </a></p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">证件号码</p>
-              <p class="p-val">{{ item.document_number }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">联系人</p>
-              <p class="p-val">{{ item.contact_name }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">职位</p>
-              <p class="p-val">{{ item.position }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">手机</p>
-              <p class="p-val">{{ item.phone }}</p>
-            </div>
-
-            <div class="item">
-              <p class="p-key">邮箱</p>
-              <p class="p-val">{{ item.email }}</p>
-            </div>
-
-          </div>
-
-          <div class="form-title">
-            <span>状态</span>
-          </div>
-
-          <div class="company-show">
-            <div class="item">
-              <p class="p-key">认证</p>
-              <p class="p-val">
-                  <span v-if="item.verify_status === 1" type="success">通过</span>
-                  <span v-else-if="item.verify_status === 2" type="gray">拒绝</span>
-                  <span v-else type="warning">待认证</span>
-              </p>
-              <p class="opt" v-if="item.verify_status === 0">
-                <el-button class="is-custom" :loading="verifyLoadingBtn" size="small" @click="setVerify(2)">拒绝</el-button>
-                <el-button type="primary" class="is-custom" :loading="verifyLoadingBtn" size="small" @click="setVerify(1)">通过</el-button>
-              </p>
-              <p class="opt" v-else>
-                <el-button class="is-custom" :loading="verifyLoadingBtn" size="small" v-if="item.verify_status === 1" @click="setVerify(2)">拒绝</el-button>
-                <el-button type="primary" class="is-custom" :loading="verifyLoadingBtn" size="small" v-else @click="setVerify(1)">通过</el-button>
-              </p>
+            <div class="admin-menu-sub-list">
+              <router-link :to="{name: 'adminCompanyList', query: {type: 1}}" :class="{'item': true, 'is-active': menuType === 1}" active-class="false">通过审核</router-link>
             </div>
           </div>
 
-        </div>
+          <div class="content-box" v-loading.body="isLoading">
 
+            <div class="form-title">
+              <span>基本信息</span>
+            </div>
+
+            <div class="company-show">
+
+              <div class="item" style="height: 90px;">
+                <p class="p-key">头像</p>
+                <p class="p-val"><img v-if="item.logo_url" :src="item.logo_url" width="80" /></p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">简称</p>
+                <p class="p-val">{{ item.company_abbreviation }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">地址</p>
+                <p class="p-val"><span v-for="(d, index) in item.city_arr" :key="index">{{ d }} &nbsp;</span> {{ item.address }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">网址</p>
+                <p class="p-val">{{ item.web }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">规模</p>
+                <p class="p-val">{{ item.company_size_val }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">分公司</p>
+                <p class="p-val">{{ item.branch_office }}</p>
+              </div>
+
+              <div class="item des">
+                <p class="p-key">公司简介</p>
+                <p class="p-val">{{ item.company_profile }}</p>
+              </div>
+
+              <div class="item des">
+                <p class="p-key">专业优势</p>
+                <p class="p-val">{{ item.professional_advantage }}</p>
+              </div>
+
+              <div class="item des">
+                <p class="p-key">荣誉奖项</p>
+                <p class="p-val">{{ item.awards }}</p>
+              </div>
+
+            </div>
+
+            <div class="form-title">
+              <span>认证信息</span>
+            </div>
+
+            <div class="company-show">
+              <div class="item">
+                <p class="p-key">企业名称</p>
+                <p class="p-val">{{ item.company_name }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">企业证件类型</p>
+                <p class="p-val">{{ item.company_type_val }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">企业营业执照</p>
+                <p class="p-val"><a v-for="(d, index) in item.license_image" :key="index" :href="d.file" target="_blank">{{ d.name }} </a></p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">注册号</p>
+                <p class="p-val">{{ item.registration_number }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">法人姓名</p>
+                <p class="p-val">{{ item.legal_person }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">法人证件类型</p>
+                <p class="p-val">{{ item.document_type_val }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">法人证件附件</p>
+                <p class="p-val"><a v-for="(d, index) in item.document_image" :key="index" :href="d.file" target="_blank">{{ d.name }} </a></p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">证件号码</p>
+                <p class="p-val">{{ item.document_number }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">联系人</p>
+                <p class="p-val">{{ item.contact_name }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">职位</p>
+                <p class="p-val">{{ item.position }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">手机</p>
+                <p class="p-val">{{ item.phone }}</p>
+              </div>
+
+              <div class="item">
+                <p class="p-key">邮箱</p>
+                <p class="p-val">{{ item.email }}</p>
+              </div>
+
+            </div>
+
+            <div class="form-title">
+              <span>状态</span>
+            </div>
+
+            <div class="company-show">
+              <div class="item">
+                <p class="p-key">认证</p>
+                <p class="p-val">
+                    <span v-if="item.verify_status === 1" type="success">通过</span>
+                    <span v-else-if="item.verify_status === 2" type="gray">拒绝</span>
+                    <span v-else type="warning">待认证</span>
+                </p>
+                <p class="opt" v-if="item.verify_status === 0">
+                  <el-button class="is-custom" :loading="verifyLoadingBtn" size="small" @click="setVerify(2)">拒绝</el-button>
+                  <el-button type="primary" class="is-custom" :loading="verifyLoadingBtn" size="small" @click="setVerify(1)">通过</el-button>
+                </p>
+                <p class="opt" v-else>
+                  <el-button class="is-custom" :loading="verifyLoadingBtn" size="small" v-if="item.verify_status === 1" @click="setVerify(2)">拒绝</el-button>
+                  <el-button type="primary" class="is-custom" :loading="verifyLoadingBtn" size="small" v-else @click="setVerify(1)">通过</el-button>
+                </p>
+              </div>
+            </div>
+
+          </div>
 
         </div>
       </el-col>
     </el-row>
-
 
   </div>
 </template>
