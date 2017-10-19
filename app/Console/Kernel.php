57e7a6e7b75efde3c5c068cc2e238bdd97ccaee8
llh <liheng666@foxmail.com>
2017-10-19 14:52:50 +0800

    change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('Weighted:calculation')->everyFiveMinutes();
     }
 
     /**
