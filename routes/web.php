dcb46099c9b82553416bc24d3fb421b52b5c990c
llh <liheng666@foxmail.com>
2017-11-09 18:41:04 +0800

    change

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::group(['namespace' => 'Web'], function () {
     Route::post('/jdCallbackUrl', 'JdCallBackController@callUrl');
