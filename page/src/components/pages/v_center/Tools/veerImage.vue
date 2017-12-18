dc26bc77fdfdb88adfa659d9ba72af0a9ccaca83
adminlte <geforce96@hotmail.com>
2017-12-18 22:20:13 +0800

    上拉加载

--- page/src/components/pages/v_center/Tools/veerImage.vue
+++ page/src/components/pages/v_center/Tools/veerImage.vue
@@ -9,7 +9,7 @@
           <el-input placeholder="请输入内容" v-model="keyword">
             <el-button slot="append" @click="getImgList(keyword)">搜索</el-button>
           </el-input>
-          <waterfall :line-gap="240" align="center" :watch="imgList"
+          <waterfall :line-gap="picWidth" align="center" :watch="imgList"
                      class="waterfall">
             <waterfall-slot
               v-for="(item, index) in imgList"
@@ -55,6 +55,9 @@
         imgList: [],
         isEmpty: false,
         isLoading: false,
+        isOk: true,
+        picWidth: 240,
+        docHeight: 0,
         pagination: {
           curPage: 1,
           pageSize: 20,
@@ -64,6 +67,21 @@
       }
     },
     created () {
+      let docWidth = document.body.offsetWidth
+      if (docWidth < 750) {
+        this.picWidth = 200
+      }
+      if (docWidth < 630) {
+        this.picWidth = 160
+      }
+      if (docWidth < 350) {
+        this.picWidth = 140
+      }
+    },
+    watch: {
+      keyword () {
+        this.pagination.curPage = 1
+      }
     },
     mounted () {
       window.addEventListener('keydown', (e) => {
@@ -72,15 +90,31 @@
             this.getImgList(this.keyword)
           } else if (e.keyCode === 37) {
             this.pagination.curPage--
-            this.handleCurChange()
+            if (this.pagination.curPage < 0) {
+              this.pagination.curPage = 0
+            }
+            this.getImgList(this.keyword)
           } else if (e.keyCode === 39) {
             this.pagination.curPage++
-            this.handleCurChange()
+            if (this.pagination.curPage > this.pagination.pageCount) {
+              this.pagination.curPage = this.pagination.pageCount
+            }
+            this.getImgList(this.keyword)
           }
         } else {
           return false
         }
       })
+      let getImg = () => {
+        if (this.isOk) {
+          let docHeight = document.body.scrollHeight
+          if (docHeight + document.body.getBoundingClientRect().top < document.body.offsetHeight + 310) {
+            this.pagination.curPage++
+            this.touchGetImgList(this.keyword)
+          }
+        }
+      }
+      window.addEventListener('touchmove', getImg, false)
     },
     methods: {
       getImgList (val) {
@@ -95,15 +129,16 @@
           this.isLoading = false
           if (res.data.meta.status_code === 200) {
             let data = res.data.data
-            console.log(res)
-            if (data.length < 1) {
-              this.isEmpty = true
-            }
-            this.imgList = data
-            if (res.data.meta.pagination.total > 20000) {
-              this.pagination.total = 20000
-            } else {
-              this.pagination.total = res.data.meta.pagination.total
+            if (data) {
+              if (data.length < 1) {
+                this.isEmpty = true
+              }
+              this.imgList = data
+              this.pagination.pageCount = res.data.meta.pagination.total_pages
+              if (this.pagination.pageCount > 1000) {
+                this.pagination.pageCount = 1000
+              }
+              this.pagination.total = this.pagination.pageSize * this.pagination.pageCount
             }
           } else {
             this.$message.error(res.data.meta.message)
@@ -113,7 +148,41 @@
           console.error(err)
         })
       },
-      handleCurChange (val) {
+      touchGetImgList (val) {
+        this.isOk = false
+        this.isLoading = true
+        this.$http.get(api.veerImage, {
+          params: {
+            page: this.pagination.curPage,
+            keyword: val,
+            nums: this.pagination.pageSize
+          }
+        }).then((res) => {
+          this.isOk = true
+          this.isLoading = false
+          if (res.data.meta.status_code === 200) {
+            let data = res.data.data
+            if (data) {
+              if (data.length < 1) {
+                this.isEmpty = true
+              }
+              this.imgList = this.imgList.concat(data)
+              if (res.data.meta.pagination.total > this.pagination.pageSize * this.pagination.pageCount) {
+                this.pagination.total = 20000
+              } else {
+                this.pagination.total = res.data.meta.pagination.total
+              }
+            }
+          } else {
+            this.$message.error(res.data.meta.message)
+          }
+        }).catch((err) => {
+          this.isOk = true
+          this.isLoading = false
+          console.error(err)
+        })
+      },
+      handleCurChange(val) {
         this.pagination.curPage = val
         this.getImgList(this.keyword)
       }
@@ -140,6 +209,10 @@
     padding: 10px;
   }
 
+  img:hover {
+    animation: move .2s 2;
+  }
+
   .pagination {
     display: flex;
     justify-content: center;
@@ -147,5 +220,38 @@
 
   .waterfall {
     margin: 20px 0;
+  }
+
+  @keyframes move {
+    10% {
+      transform: translate(4px, 0) rotate(0);
+    }
+    20% {
+      transform: translate(8px, 0) rotate(0);
+    }
+    30% {
+      transform: translate(7px, 0) rotate(0);
+    }
+    40% {
+      transform: translate(2px, 0) rotate(0);
+    }
+    50% {
+      transform: translate(0px, 0) rotate(0);
+    }
+    60% {
+      transform: translate(7px, 0) rotate(0);
+    }
+    70% {
+      transform: translate(8px, 0) rotate(0);
+    }
+    80% {
+      transform: translate(4px, 0) rotate(0);
+    }
+    90% {
+      transform: translate(2px, 0) rotate(0);
+    }
+    0%, 100% {
+      transform: translate(0, 0) rotate(0);
+    }
   }
 </style>
