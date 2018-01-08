dd9d5ec96425102b4c247f232dba1b069255d5c5
tianshuai <tianshuaiorc@sina.com>
2018-01-08 15:38:17 +0800

    admin award case

--- app/Http/Controllers/Api/Admin/AwardCaseController.php
+++ app/Http/Controllers/Api/Admin/AwardCaseController.php
@@ -104,7 +104,6 @@ class AwardCaseController extends BaseController
             'category_id' => 'required|integer',
             'cover_id' => 'required|integer',
             'tags' => 'array',
-            'time_at' => 'required',
         ];
 
         $all = $request->all();
