c762ddcdc18acda77f45881bf1f22f793b1eed1e
tianshuai <tianshuaiorc@sina.com>
2018-01-22 14:19:37 +0800

    add config vue_env

--- page/config/dev_example.env.js
+++ page/config/dev_example.env.js
@@ -2,6 +2,7 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
+  VUE_ENV: '"server"',
   NODE_ENV: '"development"',
   API_ROOT: '"http://localhost:8081/api"'
 })
