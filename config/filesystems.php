f952c2e9f004d3a5bdf8d9cbf30ec222d8d3926d
llh <liheng666@foxmail.com>
2018-03-12 11:53:44 +0800

    change

--- config/filesystems.php
+++ config/filesystems.php
@@ -84,7 +84,7 @@ return [
             'bucket' => 'yunpanmallache',
             'domain' => 'saas_yunpan',
             'call_back_url' => env('QINIU_YUNPAN_CALL_BACK_URL', 'http://sa.taihuoniao.com/yunpanCallback'),
-            'url' => 'p593eqdrg.bkt.clouddn.com/', //图片服务器
+            'url' => 'http://p593eqdrg.bkt.clouddn.com/', //图片服务器
             'small' => '-small',       //缩略图
 
             'video_small' => '?vframe/png/offset/1/w/60/h/60',  // 视频缩略图
