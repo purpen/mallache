8887a704892e860fdbc512e08d532c1f9022182e
CaiLiguang <731994627@qq.com>
2018-03-29 11:52:01 +0800

    修改邮箱唯一性判断

--- app/Http/Controllers/Api/V1/AuthenticateController.php
+++ app/Http/Controllers/Api/V1/AuthenticateController.php
@@ -440,12 +440,16 @@ class AuthenticateController extends BaseController
      */
     public function updateUser(Request $request)
     {
+        $email = $request->input('email');
         $user_id = $this->auth_user_id;
 
         $all = $request->except(['token']);
         $user = User::where('id' , $user_id)->first();
         if(!$user){
-            return $this->response->array($this->apiSuccess('没有该用户', 404));
+            return $this->response->array($this->apiError('没有该用户', 404));
+        }
+        if($user->email == $email){
+            return $this->response->array($this->apiError('邮箱不能重复', 412));
         }
         $user->update($all);
         return $this->response->array($this->apiSuccess('修改成功', 200));
