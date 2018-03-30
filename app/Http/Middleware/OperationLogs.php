832b4ad8799ba04c0eb483bb4c2a1a9fd9ffbac0
llh <liheng666@foxmail.com>
2018-03-30 17:27:16 +0800

    暂存

--- app/Http/Middleware/OperationLogs.php
+++ app/Http/Middleware/OperationLogs.php
@@ -20,8 +20,62 @@ class OperationLogs
         return $next($request);
     }
 
-    public function terminate($request, $response)
+    public function terminate(Request $request, Response $response)
     {
-        //
+        // 请求的路由
+        $path = $request->getPathInfo();
+        // 请求方法
+        $match = $request->getMethod();
+
+        foreach ($this->selfConfig() as $route => $func) {
+            if ($this->isPath($route, $match, $path)) {
+                $this->$func();
+            }
+        }
+    }
+
+    public function task(Request $request, Response $response)
+    {
+        Log::info('我是一条记录');
+    }
+
+    protected function selfConfig()
+    {
+        // ['路由:请求方法' => 对应执行的方法]
+        return [
+            'path:match' => 'task',
+        ];
+    }
+
+    /**
+     * 判断值是否是指定的路由
+     *
+     * @param $pattern string 指定的路由信息
+     * @param $match string 请求方法
+     * @param $value string 请求路由
+     *
+     * @return bool
+     */
+    public function isPath($pattern, $match, $value)
+    {
+        $pattern_arr = explode(':', $pattern);
+        if (count($pattern_arr) != 2) {
+            throw new \Exception('pattern value error');
+        }
+
+        if ($pattern_arr[1] != $match) {
+            return false;
+        }
+
+        if ($pattern_arr[0] == $value && $pattern_arr[1] == $match) {
+            return true;
+        }
+
+        $pattern = $pattern_arr[0];
+        $pattern = preg_quote($pattern, '#');
+        // 支持通配符
+        $pattern = str_replace('/*', '.*', $pattern);
+
+        return (bool)preg_match('#^' . $pattern . '\n#u', $value);
     }
 }
