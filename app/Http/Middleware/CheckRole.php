7784f9df98970da69afbf71052435d034ba8eac7
llh <liheng666@foxmail.com>
2018-05-10 15:30:58 +0800

    change

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -32,30 +32,30 @@ class CheckRole
         switch ($user->role_id){
             //普通用户
             case 0:
-                throw new HttpException(401, '无访问权限');
+                throw new HttpException(403, '无访问权限');
                 break;
             //编辑 editor
             case 5:
                 if(!$this->check($request, 'editor')){
-                    throw new HttpException(401, '无访问权限');
+                    throw new HttpException(403, '无访问权限');
                 }
                 break;
             //管理员 admin
             case 10:
                 if(!$this->check($request, 'admin')){
-                    throw new HttpException(401, '无访问权限');
+                    throw new HttpException(403, '无访问权限');
                 }
                 break;
             //管理员 admin_plus
             case 15:
                 if(!$this->check($request, 'admin_plus')){
-                    throw new HttpException(401, '无访问权限');
+                    throw new HttpException(403, '无访问权限');
                 }
                 break;
             //超级管理员 root
             case 20:
                 if(!$this->check($request, 'root')){
-                    throw new HttpException(401, '无访问权限');
+                    throw new HttpException(403, '无访问权限');
                 }
                 break;
             default:
