2cdb7ac70d26b4aeceeefb3147196d16841c3b34
llh <liheng666@foxmail.com>
2018-05-14 16:33:40 +0800

    银行转账凭证上传

--- routes/api.php
+++ routes/api.php
@@ -236,7 +236,7 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api\V1'], function ($a
         $api->get('/pay/getPayStatus/{uid}', 'PayController@getPayStatus');
 
         // api {put} /pay/bankTransfer/{pay_order_id} 银行转账凭证上传确认
-        $api->get('/pay/bankTransfer/{pay_order_id}', 'PayController@bankTransfer');
+        $api->put('/pay/bankTransfer/{pay_order_id}', 'PayController@bankTransfer');
 
         //用户提现
         $api->post('/withdraw/create', 'WithdrawOrderController@create');
