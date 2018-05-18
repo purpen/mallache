62aeba56a1bad9d5a2f4f172ecd4f9a202891c41
CaiLiguang <731994627@qq.com>
2018-05-18 18:10:59 +0800

    修改头像

--- app/Http/Transformer/StageTransformer.php
+++ app/Http/Transformer/StageTransformer.php
@@ -18,16 +18,12 @@ title	varchar(100)	是		标题
     public function transform(Stage $stage)
     {
         $tasks = Task::where('item_id' , $stage->item_id)->where('stage_id' , $stage->id)->get();
-        foreach ($tasks as $task){
-            $tasks['logo_image'] = $task->user->logo_image;
-        }
         return [
             'id' => intval($stage->id),
             'item_id' => intval($stage->item_id),
             'title' => $stage->title,
             'created_at' => $stage->created_at,
             'task' => $tasks ? $tasks : '',
-            'logo_image' => $tasks['logo_image']
         ];
     }
 }
