087a28643c6d791dd03f9f54488140cbd7a16865
llh <liheng666@foxmail.com>
2018-06-26 13:58:40 +0800

    项目详情返回发票信息

--- app/Http/Transformer/ItemStageTransformer.php
+++ app/Http/Transformer/ItemStageTransformer.php
@@ -8,32 +8,17 @@ use League\Fractal\TransformerAbstract;
 class ItemStageTransformer extends TransformerAbstract
 {
     /**
-        id	                int(10)	        否
-        item_id	            int(10)	        否		项目ID
-        design_company_id	int(10)	        否		设计公司ID
-        title	            varchar(50)	    否		阶段名称
-        content	            varchar(500)	否		内容描述
-        summary	            varcha(100)	    是	''	备注
-        status              tinyint(4)	    否	0	是否发布：0.否；1.是;
+     * id                    int(10)            否
+     * item_id                int(10)            否        项目ID
+     * design_company_id    int(10)            否        设计公司ID
+     * title                varchar(50)        否        阶段名称
+     * content                varchar(500)    否        内容描述
+     * summary                varcha(100)        是    ''    备注
+     * status              tinyint(4)        否    0    是否发布：0.否；1.是;
      */
-    public function transform(ItemStage $itemStage)
+    public function transform(ItemStage $item_stage)
     {
-        return [
-            'id' => intval($itemStage->id),
-            'item_id' => intval($itemStage->item_id),
-            'design_company_id' => intval($itemStage->design_company_id),
-            'title' => strval($itemStage->title),
-            'content' => $itemStage->array_content,
-            'summary' => strval($itemStage->summary),
-            'item_stage_image' => $itemStage->item_stage_image,
-            'status' => intval($itemStage->status),
-            'created_at' => $itemStage->created_at,
-            'percentage' => $itemStage->percentage,
-            'amount' => $itemStage->amount,
-            'time' => $itemStage->time,
-            'confirm' => $itemStage->confirm,
-            'sort' => $itemStage->sort,
-        ];
+        return $item_stage->info();
     }
 
 }
