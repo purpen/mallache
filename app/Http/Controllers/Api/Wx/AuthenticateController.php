e331d0f8bc0512614d2739eb6b175ee51736fc08
CaiLiguang <731994627@qq.com>
2018-07-23 18:02:46 +0800

    添加用户部分功能微信小程序

--- app/Http/Controllers/Api/Wx/AuthenticateController.php
+++ app/Http/Controllers/Api/Wx/AuthenticateController.php
@@ -9,6 +9,7 @@ namespace App\Http\Controllers\Api\Wx;
 
 use App\Helper\Tools;
 use App\Http\Transformer\UserTransformer;
+use App\Jobs\SendOneSms;
 use App\Models\DemandCompany;
 use App\Models\User;
 use Dingo\Api\Exception\StoreResourceFailedException;
@@ -127,7 +128,7 @@ class AuthenticateController extends BaseController
     }
 
     /**
-     * @api {post} /wechat/bindingUser 绑定用户
+     * @api {post} /wechat/bindingUser 直接绑定用户
      * @apiVersion 1.0.0
      * @apiName WxBindingUser bindingUser
      * @apiGroup Wx
@@ -172,4 +173,189 @@ class AuthenticateController extends BaseController
         }
 
     }
+
+    /**
+     * @api {post} /wechat/newBindingUser 新用户，绑定
+     * @apiVersion 1.0.0
+     * @apiName WxNewBindingUser newBindingUser
+     * @apiGroup Wx
+     *
+     * @apiParam {string} phone 手机号
+     * @apiParam {string} password 密码
+     * @apiParam {integer} sms_code 短信验证码
+     * @apiParam {string} token
+     *
+     */
+    public function newBindingUser(Request $request)
+    {
+        // 验证规则
+        $rules = [
+            'phone' => ['required', 'regex:/^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$/'],
+            'sms_code' => ['required', 'regex:/^[0-9]{6}$/'],
+            'password' => ['required', 'min:6']
+        ];
+
+
+        $payload = app('request')->only('phone', 'password', 'sms_code');
+        $validator = app('validator')->make($payload, $rules);
+
+        // 验证格式
+        if ($validator->fails()) {
+            throw new StoreResourceFailedException('请求参数格式不对！', $validator->errors());
+        }
+
+        $phone = $request->input('phone');
+        $password = $request->input('password');
+        //验证手机验证码
+        $key = 'sms_code:' . strval($payload['account']);
+        $sms_code_value = Cache::get($key);
+        if (intval($payload['sms_code']) !== intval($sms_code_value)) {
+            return $this->response->array($this->apiError('验证码错误', 412));
+        } else {
+            Cache::forget($key);
+        }
+
+        //当前登陆的用户
+        $loginUser = $this->auth_user;
+        if ($loginUser){
+            $loginUser->account = $phone;
+            $loginUser->phone = $phone;
+            $loginUser->username = $phone;
+            $loginUser->password = bcrypt($password);
+            if ($loginUser->save()){
+                return $this->response->array($this->apiSuccess('绑定成功', 200));
+            }
+
+        } else {
+            return $this->response->array($this->apiError('没有找到用户', 404));
+        }
+
+    }
+
+    /**
+     * @api {get} /wechat/phone 查看手机号是否绑定
+     * @apiVersion 1.0.0
+     * @apiName WxPhone phone
+     * @apiGroup Wx
+     *
+     * @apiParam {string} phone 手机号
+     * @apiParam {string} token
+     *
+     */
+    public function phone(Request $request)
+    {
+        // 验证规则
+        $rules = [
+            'phone' => ['required', 'regex:/^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$/'],
+        ];
+
+
+        $payload = app('request')->only('phone');
+        $validator = app('validator')->make($payload, $rules);
+
+        // 验证格式
+        if ($validator->fails()) {
+            throw new StoreResourceFailedException('请求参数格式不对！', $validator->errors());
+        }
+
+        $phone = $request->input('phone');
+        $user = User::where('account' , $phone)->first();
+        if ($user) {
+            return $this->response->array($this->apiSuccess('已经存在,可以直接绑定', 200));
+        } else {
+            return $this->response->array($this->apiError('该账户不存在,需要从新绑定', 404));
+        }
+    }
+
+
+    /**
+     * @api {post} /wechat/sms 获取手机验证码
+     * @apiVersion 1.0.0
+     * @apiName WxSmsCode sms_code
+     * @apiGroup Wx
+     *
+     * @apiParam {integer} phone
+     *
+     * @apiSuccessExample 成功响应:
+     * {
+     *     "meta": {
+     *       "message": "请求成功！",
+     *       "status_code": 200
+     *     },
+     *   }
+     */
+    public function getSmsCode(Request $request)
+    {
+        $rules = [
+            'phone' => ['required', 'regex:/^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$/'],
+        ];
+
+        $payload = $request->only('phone');
+        $validator = app('validator')->make($payload, $rules);
+
+        if ($validator->fails()) {
+            throw new StoreResourceFailedException('请求参数格式不对！', $validator->errors());
+        }
+
+        $phone = $payload['phone'];
+
+        //短信验证码加入缓存
+        $key = 'sms_code:' . strval($phone);
+        $sms_code = Tools::randNumber();
+        Cache::put($key, $sms_code, 10);
+
+        $text = ' 【太火鸟】验证码：' . $sms_code . '，切勿泄露给他人，如非本人操作，建议及时修改账户密码。';
+        //插入单条短信发送队列
+        $this->dispatch(new SendOneSms($phone, $text));
+
+        return $this->response->array($this->apiSuccess('请求成功！', 200));
+    }
+
+    /**
+     * @api {post} /wechat/changePassword 修改密码
+     * @apiVersion 1.0.0
+     * @apiName WxChangePassword changePassword
+     * @apiGroup Wx
+     *
+     * @apiParam {string} password     新密码
+     * @apiParam {string} repeatPassword     重复密码
+     * @apiParam {string} token
+     *
+     * @apiSuccessExample 成功响应:
+     * {
+     *     "meta": {
+     *       "message": "请求成功！",
+     *       "status_code": 200
+     *     },
+     *     "data": {
+     *       "token": "sdfs1sfcd"
+     *    }
+     *   }
+     */
+    public function changePassword(Request $request)
+    {
+        $this->validate($request, [
+            'repeatPassword' => 'required',
+            'password' => 'required',
+        ]);
+
+        $repeatPassword = $request->input('repeatPassword');
+        $newPassword = $request->input('password');
+
+        if ($repeatPassword != $newPassword) {
+            return $this->response->array($this->apiError('两次密码不一致', 412));
+        }
+
+        $user = JWTAuth::parseToken()->authenticate();
+
+        $user->password = bcrypt($newPassword);
+        if ($user->save()) {
+            $token = JWTAuth::refresh();
+            return $this->response->array($this->apiSuccess('请求成功', 200, compact('token')));
+        } else {
+            return $this->response->array($this->apiError('Error', 500));
+        }
+    }
+
+
 }
--- routes/wx.php
+++ routes/wx.php
@@ -8,16 +8,32 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['namespace' => 'App\Http\Controllers\Api\Wx'], function ($api) {
-        // 获取token
-        $api->get('/wechat/token', [
-            'as' => 'wechat.token', 'uses' => 'AuthenticateController@token'
-        ]);
-        //绑定用户
-        $api->post('/wechat/bindingUser', [
-            'as' => 'wechat.bindingUser', 'uses' => 'AuthenticateController@bindingUser'
-        ]);
-        //解密信息
-        $api->get('/wechat/decryptionMessage', [
-            'as' => 'wechat.decryptionMessage', 'uses' => 'AuthenticateController@decryptionMessage'
-        ]);
+    // 获取token
+    $api->get('/wechat/token', [
+        'as' => 'wechat.token', 'uses' => 'AuthenticateController@token'
+    ]);
+    //直接绑定用户
+    $api->post('/wechat/bindingUser', [
+        'as' => 'wechat.bindingUser', 'uses' => 'AuthenticateController@bindingUser'
+    ]);
+    //新用户绑定
+    $api->post('/wechat/newBindingUser', [
+        'as' => 'wechat.newBindingUser', 'uses' => 'AuthenticateController@newBindingUser'
+    ]);
+    //解密信息
+    $api->get('/wechat/decryptionMessage', [
+        'as' => 'wechat.decryptionMessage', 'uses' => 'AuthenticateController@decryptionMessage'
+    ]);
+    //检测手机号是否注册了
+    $api->get('/wechat/phone', [
+        'as' => 'wechat.phone', 'uses' => 'AuthenticateController@phone'
+    ]);
+    //获取验证码
+    $api->post('/wechat/sms', [
+        'as' => 'wechat.sms', 'uses' => 'AuthenticateController@sms'
+    ]);
+    //修改密码
+    $api->post('/wechat/changePassword', [
+        'as' => 'wechat.changePassword', 'uses' => 'AuthenticateController@changePassword'
+    ]);
 });
