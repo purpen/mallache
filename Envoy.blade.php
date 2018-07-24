8d6bd60bad27fa1a779eec0c4d7d3ddc7327259f
llh <liheng666@foxmail.com>
2018-07-24 18:28:55 +0800

    修改composer.json试支持多个php版本的环境，添加了自动部署脚本

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,7 @@
-@servers(['test_server' => 'tianxiaoyi@120.132.59.206'])
+@servers(['test_server' => 'tianxiaoyi@120.132.59.206','server_1' => 'tianxiaoyi@120.132.50.251'])
 
-# 铟果测试环境部署任务
-@task('test_server', ['on' => 'test_server'])
+# 铟果测试环境自动部署
+@task('test_server', ['on' => 'test_server', 'confirm' => true])
     cd /opt/project/mallache
     eval `ssh-agent -s`
     ssh-add ~/.ssh/herp
@@ -14,6 +14,22 @@
     /opt/php-7.0/bin/php /usr/local/bin/composer install
     /opt/php-7.0/bin/php /usr/local/bin/composer dump-autoload
     apidoc -i app/Http/Controllers/Api/ -o public/apidoc
+@endtask
+
+
+# 铟果正式环境自动部署
+@task('server', ['on' => ['server_1'], 'confirm' => true])
+cd /opt/project/mallache
+eval `ssh-agent -s`
+ssh-add ~/.ssh/herp
+@if ($branch)
+    git pull origin {{ $branch }}
+@else
+    git pull origin master
+@endif
+/opt/php-7.0/bin/php artisan migrate
+/opt/php-7.0/bin/php /usr/local/bin/composer install
+/opt/php-7.0/bin/php /usr/local/bin/composer dump-autoload
 @endtask
 
 
