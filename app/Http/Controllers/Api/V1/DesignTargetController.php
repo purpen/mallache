2ceb2ee685e4c8461a0f2693ea64a9a65fd848f1
CaiLiguang <731994627@qq.com>
2018-07-27 18:15:46 +0800

    添加统计图

--- app/Http/Controllers/Api/V1/DesignTargetController.php
+++ app/Http/Controllers/Api/V1/DesignTargetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 
 use App\Http\Transformer\DesignTargetTransformer;
+use App\Models\DesignProject;
 use App\Models\DesignTarget;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -13,7 +14,7 @@ class DesignTargetController extends BaseController
 {
 
     /**
-     * @api {post} /designTarget/store 计划年营业额，年度项目数量添加更新
+     * @api {post} /designTarget/create 计划年营业额，年度项目数量添加更新
      * @apiVersion 1.0.0
      * @apiName designTarget store
      * @apiGroup designTarget
@@ -22,15 +23,15 @@ class DesignTargetController extends BaseController
      * @apiParam {integer} count 项目数量
      * @apiParam {string} token
      */
-    public function store(Request $request)
+    public function create(Request $request)
     {
         //获取当年是那一年
-        $year = strtotime(date('Y'));
         $user_id = $this->auth_user_id;
         $design_company_id = User::designCompanyId($user_id);
         $turnover = $request->input('turnover');
         $count = $request->input('count');
-        $design_target = DesignTarget::where('year' , $year)->where('design_company_id' , $design_company_id)->first();
+        $design_target = DesignTarget::where('design_company_id' , $design_company_id)->whereYear('created_at', date('Y'))
+            ->first();
         //检查是否是管理员以上级别
         if (!$this->auth_user->isDesignAdmin()) {
             return $this->response->array($this->apiError('没有权限创建', 403));
@@ -51,13 +52,11 @@ class DesignTargetController extends BaseController
             //营销额不是空的话，保存
             if (!empty($turnover)){
                 $new_design_target->turnover = $turnover;
-                $new_design_target->year = $year;
                 $new_design_target->design_company_id = $design_company_id;
                 $new_design_target->save();
             }
             if ($count != 0){
                 $design_target->count = $count;
-                $new_design_target->year = $year;
                 $new_design_target->design_company_id = $design_company_id;
                 $design_target->save();
             }
@@ -73,29 +72,127 @@ class DesignTargetController extends BaseController
      * @apiGroup designTarget
      *
      * @apiParam {string} token
+     *
+     * @apiSuccessExample 成功响应:
+    {
+    "data": {
+    "id": 1,
+    "count": 2,
+    "design_company_id": 49,
+    "turnover": 100,
+    "year": 2018,
+    "total_item_counts": 2, //项目总数
+    "item_counts": 2, //当年完成的项目数
+    "no_item_counts": 0,         //当年没有完成的项目数
+    "ok_count_percentage": 100, //完成的项目百分比
+    "no_count_percentage": 0, //没完成的项目百分比
+    "ok_turnover_percentage": 2, //营业额百分比
+    "m_money": 0.29, //月均收入
+    "month_on_month": 0, //月环比
+    "m_item": 0.3 //月均项目
+    },
+    "meta": {
+    "message": "Success",
+    "status_code": 200
+    }
+    }
      */
     public function show()
     {
         //获取当年是那一年
-        $year = strtotime(date('Y'));
-        dd($year);
         $user_id = $this->auth_user_id;
         $design_company_id = User::designCompanyId($user_id);
-        $design_target = DesignTarget::where('year' , $year)->where('design_company_id' , $design_company_id)->first();
+        $design_target = DesignTarget::where('design_company_id' , $design_company_id)->whereYear('created_at', date('Y'))->first();
 
-        //获取当年的完成项目数量
-        $item_count = DB::table('design_project')
-            ->where('design_company_id', $design_company_id)
-            ->where('pigeonhole', 1)
+        //获取项目总数
+       $total_item_counts = DesignProject
+            ::where('design_company_id', $design_company_id)
             ->whereYear('created_at', date('Y'))
             ->count();
-        //已完成的项目百分比
-        dd($design_target);
-        if ($design_target->count == 0){
-            $ok_count = 0 ;
-        } else {
-            $ok_count = round(($item_count / $design_target->count) * 100, 0);
+        //获取当年的完成项目数量
+        $item_counts =  DesignProject
+            ::where('design_company_id', $design_company_id)
+            ->where('pigeonhole', 1)
+            ->whereYear('created_at', date('Y'))
+            ->get();
+        //没有归档的项目数
+        $no_item_counts = $total_item_counts - $item_counts->count();
+        //获取当年完成的营业额
+        $turnover = 0;
+        foreach ($item_counts as $item_count){
+            $turnover += $item_count->cost;
         }
-        dd($ok_count);
+        //已完成的项目百分比
+        if ($design_target->count == 0){
+            $ok_count_percentage = 0 ;
+        } else {
+            $ok_count_percentage = round(($item_counts->count() / $design_target->count) * 100, 0);
+        }
+        //未完成的项目百分比
+        $no_count_percentage = 100 - $ok_count_percentage;
+        //已收入的百分比
+        if ($turnover == 0){
+            $ok_turnover_percentage = 0;
+        } else {
+            $ok_turnover_percentage = round(($turnover / $design_target->turnover) * 100, 0);
+        }
+        //月均收入
+        if ($turnover == 0){
+            $m_money = 0;
+        } else {
+            $m_money =  round(($turnover / date('m')),2);
+        }
+        //月均项目
+        if ($item_counts->count() == 0){
+            $m_item = 0;
+        } else {
+            $m_item =  round(($item_counts->count() / date('m')),1);
+        }
+
+        //当月完成的项目
+        $month_item_counts = DesignProject
+            ::where('design_company_id', $design_company_id)
+            ->where('pigeonhole', 1)
+            ->whereMonth('created_at', date('m'))
+            ->get();
+        //当月项目收入
+        $current_m_money = 0;
+        foreach ($month_item_counts as $month_item_count){
+            $current_m_money += $month_item_count->cost;
+        }
+        //去年这个月完成的项目
+        $last_month_item_counts = DesignProject
+            ::where('design_company_id', $design_company_id)
+            ->where('pigeonhole', 1)
+            ->whereMonth('created_at', date('m')-1)
+            ->get();
+        //上月项目收入
+        $last_current_m_money = 0;
+        foreach ($last_month_item_counts as $last_month_item_count){
+            $last_current_m_money += $last_month_item_count->cost;
+        }
+        //月环比
+        $month_on_month = round((($current_m_money - $last_current_m_money) / 100 ) * 100 , 0);
+        //项目总数
+        $design_target['total_item_counts'] = $total_item_counts;
+        //当年完成的项目数
+        $design_target['item_counts'] = $item_counts->count();
+        //当年没有完成的项目数
+        $design_target['no_item_counts'] = $no_item_counts;
+        //完成的项目百分比
+        $design_target['ok_count_percentage'] = $ok_count_percentage;
+        //没完成的项目百分比
+        $design_target['no_count_percentage'] = $no_count_percentage;
+        //营业额百分比
+        $design_target['ok_turnover_percentage'] = $ok_turnover_percentage;
+        //月均收入
+        $design_target['m_money'] = $m_money;
+        //月环比
+        $design_target['month_on_month'] = $month_on_month;
+        //月均项目
+        $design_target['m_item'] = $m_item;
+
+        return $this->response->item($design_target, new DesignTargetTransformer())->setMeta($this->apiMeta());
+
     }
 }
--- app/Http/Transformer/DesignTargetTransformer.php
+++ app/Http/Transformer/DesignTargetTransformer.php
@@ -19,6 +19,16 @@ class DesignTargetTransformer extends TransformerAbstract
             'design_company_id' => intval($designTarget->design_company_id),
             'turnover' => intval($designTarget->turnover),
             'year' => $designTarget->year ? (int)date('Y',$designTarget->year) : 0,
+            'total_item_counts' => $designTarget->total_item_counts,
+            'item_counts' => $designTarget->item_counts,
+            'no_item_counts' => $designTarget->no_item_counts,
+            'ok_count_percentage' => $designTarget->ok_count_percentage,
+            'no_count_percentage' => $designTarget->no_count_percentage,
+            'ok_turnover_percentage' => $designTarget->ok_turnover_percentage,
+            'm_money' => $designTarget->m_money,
+            'month_on_month' => $designTarget->month_on_month,
+            'm_item' => $designTarget->m_item,
+
         ];
     }
 
