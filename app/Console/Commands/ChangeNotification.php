af7768cc804b6d4bf89cdce7f32c735aea3a6f55
CaiLiguang <731994627@qq.com>
2018-08-09 18:23:07 +0800

    通知缺少参数的问题

--- app/Console/Commands/ChangeNotification.php
+++ app/Console/Commands/ChangeNotification.php
@@ -66,8 +66,9 @@ class ChangeNotification extends Command
         foreach ($q_item_recommends as $q_item_recommend){
             $q_design_company = DesignCompanyModel::where('id' , $q_item_recommend->design_company_id)->first();
             if($q_design_company){
+                $q_content = '需求方已选择贵公司，请您尽快报价';
                 Log::info($q_design_company->phone);
-                Tools::sendSmsToPhone($q_design_company->phone);
+                Tools::sendSmsToPhone($q_design_company->phone , $q_content);
             }
         }
         $this->info('报价通知完毕');
@@ -96,8 +97,9 @@ class ChangeNotification extends Command
         foreach ($c_item_recommends as $c_item_recommend){
             $c_design_company = DesignCompanyModel::where('id' , $c_item_recommend->design_company_id)->first();
             if($c_design_company){
+                $c_content = '需求方已选择贵公司，请您尽快填写合同';
                 Log::info($c_design_company->phone);
-                Tools::sendSmsToPhone($c_design_company->phone);
+                Tools::sendSmsToPhone($c_design_company->phone , $c_content);
             }
         }
         $this->info('合同通知完毕');
