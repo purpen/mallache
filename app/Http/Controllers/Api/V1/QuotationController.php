3a1c05f3b46aec7ec307fc33411f711fcdc7681e
llh <liheng666@foxmail.com>
2018-08-23 18:46:56 +0800

    debug

--- app/Http/Controllers/Api/V1/QuotationController.php
+++ app/Http/Controllers/Api/V1/QuotationController.php
@@ -188,9 +188,9 @@ class QuotationController extends BaseController
                 // 基础报价不含税
                 $a = ($quotation_info['total_price'] * $quotation_info['tax_rate']) / 100;
 
-                Log::info(1111111);
-                Log::info(round($quotation_info['price'], 2));
-                Log::info(round(((float)$quotation_info['total_price'] + $a), 2));
+                Log::error(1111111);
+                Log::error(round($quotation_info['price'], 2));
+                Log::error(round(((float)$quotation_info['total_price'] + $a), 2));
 
                 if (round($quotation_info['price'], 2) != round(((float)$quotation_info['total_price'] + $a), 2)) {
                     throw new MassageException('合计金额和总计金额不符', 403);
