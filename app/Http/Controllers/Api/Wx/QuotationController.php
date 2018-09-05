e8a4179f571d8ad63faf9b53d5560a06db47a4c7
yht123799009 <123799009@qq.com>
2018-09-05 18:07:39 +0800

    智能报价去掉用户ID

--- app/Http/Controllers/Api/Wx/QuotationController.php
+++ app/Http/Controllers/Api/Wx/QuotationController.php
@@ -22,14 +22,13 @@ use App\Http\AdminTransformer\DemandCompanyTransformer;
 class QuotationController extends BaseController
 {
     /**
-     * @api {post} /wechat/quotationAdd  查看报价
+     * @api {post} /wechat/quotationAdd  智能报价
      * @apiVersion 1.0.0
      * @apiName WxquotationAdd quotationAdd
      * @apiGroup Wx
      *
-     * @apiParam {integer} user_id 用户ID
      * @apiParam {integer} type 设计类型：1.产品设计；2.UI UX 设计；3. 平面设计 4.H5 5.包装设计 6.插画设计
-     * @apiParam {json} design_types 设计类别：产品设计（1.产品策略；2.产品设计；3.结构设计；）UXUI设计（1.app设计；2.网页设计；3.'界面设计', 4 . '服务设计', 5 . '用户体验咨询'）平面设计（1.'logo/VI设计', 2.'海报/宣传册', 3 .'画册/书装'）H5(1.H5) 包装设计（1.包装设计）插画（1. '商业插画', 2. '书籍插画', 3. '形象/IP插画'）。[1,2]
+     * @apiParam {json} design_types 设计类别：产品设计（1.产品策略；2.产品设计；3.结构设计；）UXUI设计（1.app设计；2.网页设计；3.'界面设计', 4 . '服务设计', 5 . '用户体验咨询'）平面设计（1.'logo/VI设计', 2.'海报/宣传册', 3 .'画册/书装'）H5(1.H5) 包装设计（1.包装设计）插画（1. '商业插画', 2. '书籍插画', 3. '形象/IP插画'）
      * @apiParam {integer} level 级别：1. 一般 2. 较高 3.优质
      */
 
@@ -37,13 +36,12 @@ class QuotationController extends BaseController
     {
         // 验证规则
         $rules = [
-            'user_id' => 'required|integer',
             'type' => 'required|integer|regex:/[1-6]/',
             'design_types' => 'JSON',
             'level' => 'required|integer|regex:/[1-3]/',
         ];
 
-        $payload = $request->only('user_id', 'type',  'design_types','level');
+        $payload = $request->only('type',  'design_types','level');
         $validator = app('validator')->make($payload, $rules);
 
         // 验证格式
@@ -54,7 +52,7 @@ class QuotationController extends BaseController
         //获取数据
         $req = $request->all();
         $baojia = new ItemQuotation();
-        $baojia->user_id = $req['user_id'];
+        $baojia->user_id = 0;
         $baojia->type = $req['type'];
         $baojia->design_types = $req['design_types'];
         $baojia->level = $req['level'];
