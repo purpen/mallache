c40d7427d373cb938b522f7fc262017ed9ddbb19
CaiLiguang <731994627@qq.com>
2018-09-20 14:18:15 +0800

    打印用户信息

--- app/Http/Controllers/Api/Wx/AuthenticateController.php
+++ app/Http/Controllers/Api/Wx/AuthenticateController.php
@@ -44,6 +44,7 @@ class AuthenticateController extends BaseController
 
         //获取openid和session_key
         $new_mini = $mini->auth->session($code);
+        Log::info($new_mini);
         $openid = $new_mini['openid'] ?? '';
         if (!empty($openid)) {
             $wxUser = User::where('wx_open_id', $openid)->first();
