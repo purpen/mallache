a90102018d5bcbef709327468864efc01ae0b76b
CaiLiguang <731994627@qq.com>
2018-10-15 16:53:24 +0800

    修改类名

--- app/Http/Controllers/Api/Wx/DesignCaseController.php
+++ app/Http/Controllers/Api/Wx/DesignCaseController.php
@@ -3,13 +3,14 @@
  * Created by PhpStorm.
  * User: cailiguang
  * Date: 2018/10/15
- * Time: 下午3:51
+ * Time: 下午4:40
  */
 namespace App\Http\Controllers\Api\Wx;
 
+
+use Illuminate\Http\Request;
 use App\Http\Transformer\DesignCaseListsTransformer;
 use App\Models\DesignCaseModel;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DesignCaseController extends BaseController
@@ -17,7 +18,7 @@ class DesignCaseController extends BaseController
     /**
      * @api {get} /wechat/designCase 设计案例搜索项目名称
      * @apiVersion 1.0.0
-     * @apiName designCase designCase
+     * @apiName designCase lists
      * @apiGroup wechatDemandType
      *
      * @apiParam {string} item_name
@@ -26,12 +27,12 @@ class DesignCaseController extends BaseController
     {
         $item_name = $request->input('item_name');
         //模糊查询有的话，走上面，没有的话走下面
-        $designCases = DesignCaseModel::where('item_name' , 'like', '%' . $item_name . '%')->limit(10)->get();
+        $designCases = DesignCaseModel::where('title' , 'like', '%' . $item_name . '%')->limit(10)->get();
         if($designCases){
             return $this->response->collection($designCases, new DesignCaseListsTransformer())->setMeta($this->apiMeta());
         }
         $design_cases = DesignCaseModel::
-            orderBy(DB::raw('RAND()'))
+        orderBy(DB::raw('RAND()'))
             ->take(10)
             ->get();
         return $this->response->collection($design_cases, new DesignCaseListsTransformer())->setMeta($this->apiMeta());
