3f48b8a6ccf05fbe372c579b400d58e7e41f9998
yht123799009 <123799009@qq.com>
2018-10-29 16:14:29 +0800

    交易会修改设计公司查看某个需求详情

--- app/Http/Controllers/Api/Sd/DesignDemandController.php
+++ app/Http/Controllers/Api/Sd/DesignDemandController.php
@@ -488,8 +488,7 @@ class DesignDemandController extends BaseController
 //        if(!$design_company->isVerify()){
 //            return $this->response->array($this->apiError('设计公司没有认证', 403));
 //        }
-
-
+        
         $demand_info = DesignDemand::where('id', $demand_id)->first();
         if (!$demand_info) {
             return $this->response->array($this->apiError('没有找到该需求', 404));
