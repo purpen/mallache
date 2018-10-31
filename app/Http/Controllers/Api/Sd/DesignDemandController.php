6a9c97d4a5412165a9d62e062b0cec7f0766ba22
yht123799009 <123799009@qq.com>
2018-10-31 15:40:12 +0800

    交易会修改需求列表信息

--- app/Http/Controllers/Api/Sd/DesignDemandController.php
+++ app/Http/Controllers/Api/Sd/DesignDemandController.php
@@ -410,10 +410,8 @@ class DesignDemandController extends BaseController
         // 设计公司获取需求列表
         $demandIds = DesignDemand::getCollectDemandId($design_company_id);
         $design_demand = DesignDemand::query()
-            ->join('users','users.id','=','design_demand.user_id')
-            ->join('demand_company','demand_company.id','=','design_demand.demand_company_id')
+            ->with('DemandCompany','User')
             ->where('design_demand.status', 2)->paginate($per_page);
-
         // 判断是否关注
         if(!$demandIds){
             foreach ($design_demand as $v){
