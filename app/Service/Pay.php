40637482cd1cd87f4042d9e736d57e5d052bf65c
wangsong <13217229788@163.com>
2018-11-09 09:33:35 +0800

    添加日志

--- app/Service/Pay.php
+++ app/Service/Pay.php
@@ -164,10 +164,14 @@ class Pay
         $demand_company = DemandCompany::where('user_id',$this->pay_order->design_user_id)->first();
         //修改设计成果状态为已付款并下架
         $design_result->status = -1;
+        //修改为已出售
         $design_result->sell = 1;
+        //购买需求公司ID
         $design_result->demand_company_id = $demand_company->id;
+        //购买用户ID
         $design_result->purchase_user_id = $this->pay_order->design_user_id;
         $design_result->save();
+        Log::info($design_result);
         //关闭所有设计成果未支付订单
         $this->ClosePayOrders($this->pay_order->design_result_id);
     }
