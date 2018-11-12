7b6a3bd4ce3f4d562ef813a510b3f998b29e9dce
CaiLiguang <731994627@qq.com>
2018-11-12 16:05:55 +0800

    打印

--- app/Http/Controllers/Api/Jd/JdAccountController.php
+++ app/Http/Controllers/Api/Jd/JdAccountController.php
@@ -33,7 +33,6 @@ class JdAccountController extends BaseController
         //获取access_token
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $date = curl_exec($curl);
         curl_close($curl);
