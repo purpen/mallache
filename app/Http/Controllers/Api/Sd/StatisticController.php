f7ff8419f9c5a1deadc374ac3e259f5953684bef
CaiLiguang <731994627@qq.com>
2018-11-15 11:44:56 +0800

    修改访问量

--- app/Http/Controllers/Api/Sd/StatisticController.php
+++ app/Http/Controllers/Api/Sd/StatisticController.php
@@ -8,55 +8,70 @@
 namespace App\Http\Controllers\Api\Sd;
 
 use App\Models\Statistic;
+use Illuminate\Http\Request;
 
 class StatisticController extends BaseController
 {
     /**
-     * @api {get} /sd/tourist  游客数量
+     * @api {get} /sd/touristWilling  游客,志向数量
      * @apiVersion 1.0.0
-     * @apiName statistics touristAdd
+     * @apiName statistics touristWilling
      * @apiGroup statistics
+     *
+     * @apiParam {integer} type 默认1 1.游客 2.有志向
      */
-    public function tourist()
+    public function touristWilling(Request $request)
     {
-        $statistic = Statistic::find(1);
-        if($statistic){
-            $tourist = $statistic->tourist;
-            $tourist += 1;
-            $new_tourist = $tourist;
-            $statistic->tourist = $new_tourist;
-            $statistic->save();
-            return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
-        }else{
-            $statistic = new Statistic();
-            $statistic->tourist = 1;
-            $statistic->save();
-            return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+        $type = $request->input('type') ?? 1;
+        if ($type == 1) {
+            $statistic = Statistic::find(1);
+            if ($statistic) {
+                $tourist = $statistic->tourist_count;
+                $tourist += 1;
+                $new_tourist = $tourist;
+                $statistic->tourist_count = $new_tourist;
+                $statistic->save();
+                return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+            } else {
+                $statistic = new Statistic();
+                $statistic->tourist_count = 1;
+                $statistic->save();
+                return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+            }
+        } else {
+            $statistic = Statistic::find(1);
+            if ($statistic) {
+                $willing = $statistic->willing_count;
+                $willing += 1;
+                $new_willing = $willing;
+                $statistic->willing_count = $new_willing;
+                $statistic->save();
+                return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+            } else {
+                $statistic = new Statistic();
+                $statistic->willing_count = 1;
+                $statistic->save();
+                return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+            }
         }
+
     }
 
     /**
-     * @api {get} /sd/willing  有志向数量
+     * @api {get} /sd/statistics  展示
      * @apiVersion 1.0.0
-     * @apiName statistics willingAdd
+     * @apiName statistics index
      * @apiGroup statistics
      */
-    public function willing()
+    public function index()
     {
         $statistic = Statistic::find(1);
-        if($statistic){
-            $willing = $statistic->willing;
-            $willing += 1;
-            $new_willing = $willing;
-            $statistic->willing = $new_willing;
-            $statistic->save();
-            return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
-        }else{
-            $statistic = new Statistic();
-            $statistic->willing = 1;
-            $statistic->save();
-            return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+        if(!$statistic){
+            $statistic['tourist_count'] = 0;
+            $statistic['willing_count'] = 0;
         }
+        return $this->response->array($this->apiSuccess('获取成功' , 200 , $statistic));
+
     }
 
 }
--- routes/sd.php
+++ routes/sd.php
@@ -12,13 +12,13 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api\Sd'], function ($a
      *
      * 需验证用户token
      */
-    //游客数量
-    $api->get('/sd/tourist', [
-        'as' => 'sd.touristCount', 'uses' => 'StatisticController@tourist'
+    //游客数量,有志向数量
+    $api->get('/sd/touristWilling', [
+        'as' => 'sd.touristWillingCount', 'uses' => 'StatisticController@touristWilling'
     ]);
-    //有志向数量
-    $api->get('/sd/willing', [
-        'as' => 'sd.willingCount', 'uses' => 'StatisticController@willing'
+    //展示数量
+    $api->get('/sd/statistics', [
+        'as' => 'sd.touristWillingIndex', 'uses' => 'StatisticController@index'
     ]);
     $api->group(['middleware' => ['jwt.auth']], function ($api) {
         /**
