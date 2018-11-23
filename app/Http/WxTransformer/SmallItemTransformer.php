ce5e5a48a491366834eba2679c033b25f04ef54d
CaiLiguang <731994627@qq.com>
2018-11-23 15:59:29 +0800

    修改

--- app/Http/WxTransformer/SmallItemTransformer.php
+++ app/Http/WxTransformer/SmallItemTransformer.php
@@ -10,8 +10,14 @@ class SmallItemTransformer extends TransformerAbstract
     public function transform(SmallItem $smallItem)
     {
         $s_user = substr($smallItem->user_name , 0 , 1);
-        $start_phone = substr($smallItem->phone , 0 , 3);
-        $end_phone = substr($smallItem->phone , 7);
+        if(strlen($smallItem->phone) < 11){
+            $new_phone = '139****6866';
+        }else{
+            $start_phone = substr($smallItem->phone , 0 , 3);
+            $end_phone = substr($smallItem->phone , 7);
+            $new_phone = $start_phone.'****'.$end_phone;
+        }
+
         return [
             'id' => $smallItem->id,
             'status' => $smallItem->status,
@@ -20,7 +26,7 @@ class SmallItemTransformer extends TransformerAbstract
             'phone' => $smallItem->phone,
             'created_at' => $smallItem->created_at,
             'new_user_name' => $s_user.'用户',
-            'new_phone' => $start_phone.'****'.$end_phone,
+            'new_phone' => $new_phone,
         ];
     }
 
