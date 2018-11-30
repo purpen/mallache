b0c972ac649fccfb4486d436379e5f3854215d8b
llh <liheng666@foxmail.com>
2018-11-30 13:57:33 +0800

    修改部署脚本

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,20 +1,18 @@
-@servers([
-'test_server' => 'tianxiaoyi@120.132.59.206',
-'server_1' => 'tianxiaoyi@120.132.50.251',
-])
+@servers(['test_server' => 'tianxiaoyi@120.132.59.206','server_1' => 'tianxiaoyi@120.132.50.251',])
 
 # 铟果测试环境自动部署
 @task('test_server', ['on' => 'test_server', 'confirm' => true])
 cd /opt/project/mallache
-set-ssh
+eval `ssh-agent -s`
+ssh-add ~/.ssh/herp
 @if ($branch)
     git pull origin {{ $branch }}
 @else
     git pull origin master
 @endif
 php artisan migrate
-php composer install
-php composer dump-autoload
+composer install
+composer dump-autoload
 apidoc -i app/Http/Controllers/Api/ -o public/apidoc
 @endtask
 
@@ -22,13 +20,14 @@ apidoc -i app/Http/Controllers/Api/ -o public/apidoc
 # 铟果正式环境自动部署
 @task('server', ['on' => ['server_1'], 'confirm' => true])
 cd /opt/project/mallache
-set-ssh
+eval `ssh-agent -s`
+ssh-add ~/.ssh/herp
 @if ($branch)
     git pull origin {{ $branch }}
 @else
     git pull origin master
 @endif
 php artisan migrate
-php composer install
-php composer dump-autoload
+composer install
+composer dump-autoload
 @endtask
